a8db73073443cfb736483abda107a1e6c37573e9
hisazakura <luthfanarrayyan@gmail.com>
2024-03-27 15:59:39 +0700

    Add cors

--- RAG/load.py
+++ RAG/load.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify,Response
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
 from langchain.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 from pathlib import Path
@@ -11,44 +12,45 @@ from api_activation import openai_activate
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app, origins=[os.getenv('CLIENT_ORIGIN')]) # permit cors to client app
 
 try:
     path = os.path.dirname(os.path.abspath(__file__))
-    upload_folder=os.path.join(
-    path.replace("/file_folder",""),"tmp")
+    upload_folder = os.path.join(path.replace("/file_folder", ""), "tmp")
     os.makedirs(upload_folder, exist_ok=True)
-    app.config['upload_folder'] = upload_folder
+    app.config["upload_folder"] = upload_folder
 except Exception as e:
     app.logger.info("An error occurred while creating temp folder")
     app.logger.error("Exception occurred : {}".format(e))
 
-                     
-@app.route('/')
+
+@app.route("/")
 def index():
-    return Response(json.dumps({
-    "status": True,
-    "code": 200,
-    "message": "Its Working!"}), mimetype="application/json")
+    return Response(
+        json.dumps({"status": True, "code": 200, "message": "Its Working!"}),
+        mimetype="application/json",
+    )
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def post():
     try:
         openai_activate.activation()
         path = os.path.dirname(os.path.abspath(__file__))
-        upload_folder=os.path.join(
-        path.replace("/file_folder",""),"tmp")
+        upload_folder = os.path.join(path.replace("/file_folder", ""), "tmp")
         os.makedirs(upload_folder, exist_ok=True)
-        app.config['upload_folder'] = upload_folder
-        pdf_file = request.files['file']
+        app.config["upload_folder"] = upload_folder
+        pdf_file = request.files["file"]
         pdf_name = pdf_file.filename
-        save_path = os.path.join('RAG/testing_pdf/',pdf_name)
+        save_path = os.path.join("RAG/testing_pdf/", pdf_name)
         pdf_file.save(save_path)
         loader = PyPDFLoader(save_path)
         loader.load()
-        return jsonify({'message': 'PDF file uploaded and loaded successfully'})
+        return jsonify({"message": "PDF file uploaded and loaded successfully"})
     except Exception as e:
-        app.logger.info("error occurred")
+        app.logger.error(f"Error occurred: {str(e)}")
+        return jsonify({"error": "Failed to upload and process the PDF file"}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
