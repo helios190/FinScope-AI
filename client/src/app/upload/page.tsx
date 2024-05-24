bf2dddf74e980b9f7cfd83b4d91348d46fbf5f46
hisazakura <luthfanarrayyan@gmail.com>
2024-05-24 23:15:23 +0700

    update

--- client/src/app/upload/page.tsx
+++ client/src/app/upload/page.tsx
@@ -6,10 +6,30 @@ import DropFile from "@/components/upload_page/DropFile";
 import Link from "next/link";
 import QuestionCircle from "@/components/icons/QuestionCircle";
 import { useRouter } from "next/navigation";
+import Button from "@/components/basic/Button";
+import Trash from "@/components/icons/TrashFill";
+
+enum Features {
+  Feature1,
+  Feature2,
+}
+
+interface CompanyData {
+  name: string;
+}
 
 export default function UploadPage() {
   const router = useRouter();
+
+  const [feature, setFeature] = useState<Features>(Features.Feature2);
   const [file, setFile] = useState<File | undefined>(undefined);
+  const [companies, setCompanies] = useState<CompanyData[]>([
+    { name: "BNI" },
+    { name: "BRI" },
+    { name: "BCA" },
+    { name: "Mandiri" },
+  ]);
+  const [selectedCompanies, setSelectedCompanies] = useState<(CompanyData | null)[]>([null]);
 
   const handleFileSelect = (file: File | undefined) => {
     setFile(file);
@@ -33,55 +53,121 @@ export default function UploadPage() {
     const formData = new FormData();
     formData.append("file", file);
     try {
-      const response = await fetch(/*process.env.NEXT_PUBLIC_BACKEND_API_ORIGIN + "/upload"*/ "/api/upload", {
+      const response = await fetch("/api/upload", {
         method: "POST",
         body: await file.arrayBuffer(),
       });
 
-      if (response.ok)  {
+      if (response.ok) {
         const data = await response.json();
-        if (data.id)
-            router.push("/result?id=" + data.id);
-      }
-      else alert("Upload failed");
+        if (data.id) router.push("/result?id=" + data.id);
+      } else alert("Upload failed");
     } catch (e) {
       console.error("Error:", e);
       alert("Error uploading file");
     }
   };
+  const handleCompanyChange = (index: number, event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCompanies([...selectedCompanies].with(index, companies.find((company) => company.name === event.target.value) || null));
+  };
+  const handleRemoveCompany = (index: number) => {
+    setSelectedCompanies(selectedCompanies.filter((_, i) => i !== index));
+  };
 
   return (
-    <div className="flex flex-col p-12 gap-y-2">
-      <p className="font-bold text-lg">Upload Financial Report</p>
-      <BrowseFile file={file} onFileSelect={handleFileSelect} />
-      <DropFile onFileDrop={handleFileDrop} />
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-row gap-x-8">
-          <div className="flex flex-row gap-x-2">
-            <input type="checkbox" className="w-4 accent-neutral-800"></input>
-            <p className="text-lg">Option 1</p>
-          </div>
-          <div className="flex flex-row gap-x-2">
-            <input type="checkbox" className="w-4 accent-neutral-800"></input>
-            <p className="text-lg">Option 2</p>
+    <>
+      <div className="flex flex-row w-full justify-center px-12 pt-12">
+        <button
+          className={`w-full border-2 border-black p-2 rounded-l-md ${
+            feature == Features.Feature1 ? "text-white bg-black" : "text-black bg-white"
+          }`}
+          onClick={() => {
+            if (feature != Features.Feature1) setFeature(Features.Feature1);
+          }}
+        >
+          Feature 1
+        </button>
+        <button
+          className={`w-full border-2 border-black p-2 rounded-r-md ${
+            feature == Features.Feature2 ? "text-white bg-black" : "text-black bg-white"
+          }`}
+          onClick={() => {
+            if (feature != Features.Feature2) setFeature(Features.Feature2);
+          }}
+        >
+          Feature 2
+        </button>
+      </div>
+      {feature == Features.Feature1 && (
+        <div className="flex flex-col px-12 pt-6 pb-12 gap-y-2">
+          <p className="font-bold text-lg">Upload Financial Report</p>
+          <BrowseFile file={file} onFileSelect={handleFileSelect} />
+          <DropFile onFileDrop={handleFileDrop} />
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col sm:flex-row gap-x-8">
+              <div className="flex flex-row gap-x-2">
+                <input type="checkbox" className="w-4 accent-neutral-800"></input>
+                <p className="text-lg">Option 1</p>
+              </div>
+              <div className="flex flex-row gap-x-2">
+                <input type="checkbox" className="w-4 accent-neutral-800"></input>
+                <p className="text-lg">Option 2</p>
+              </div>
+            </div>
+            {/* example pdf if needed */}
+            <Link href="#">
+              <div className="flex flex-row gap-x-2 items-center">
+                <QuestionCircle width={20} height={20} />
+                {/* <Image src="./icons/question-circle.svg" width={20} height={20} alt="question-mark" /> */}
+                <p className="text-lg">Example PDF</p>
+              </div>
+            </Link>
           </div>
         </div>
-        {/* example pdf if needed */}
-        <Link href="#">
-          <div className="flex flex-row gap-x-2 items-center">
-            <QuestionCircle width={20} height={20} />
-            {/* <Image src="./icons/question-circle.svg" width={20} height={20} alt="question-mark" /> */}
-            <p className="text-lg">Example PDF</p>
-          </div>
-        </Link>
+      )}
+      {feature == Features.Feature2 && (
+        <div className="flex flex-col px-12 pt-6 pb-12 gap-y-2 items-center">
+          {selectedCompanies.map((company, i) => {
+            return (
+              <div key={i} className="flex flex-row w-full border-2 rounded-md border-black overflow-hidden">
+                <select
+                  className="w-full p-4 mr-2 outline-none"
+                  value={company?.name ?? companies[0].name}
+                  onChange={(event) => handleCompanyChange(i, event)}
+                >
+                  {companies.map((company, i) => {
+                    return <option key={i}>{company?.name ?? ""}</option>;
+                  })}
+                </select>
+                <button className="px-4 bg-red-600" onClick={() => handleRemoveCompany(i)}>
+                    <Trash fill="white"/>
+                </button>
+              </div>
+            );
+          })}
+          <button
+            className="rounded-md p-2 border-2 border-black hover:bg-gray-300 w-1/4"
+            hidden={selectedCompanies.length >= 5}
+            onClick={() => {
+              if (selectedCompanies.length < 5) setSelectedCompanies([...selectedCompanies, null]);
+            }}
+          >
+            Add
+          </button>
+        </div>
+      )}
+      <div className="px-12">
+        <button
+          className="text-white bg-neutral-800 w-full py-2 rounded-lg enabled:hover:bg-neutral-900 disabled:opacity-50"
+          disabled={
+            (feature == Features.Feature1 && file == undefined) ||
+            (feature == Features.Feature2 && selectedCompanies.length < 2)
+          }
+          onClick={handleSubmit}
+        >
+          Upload
+        </button>
       </div>
-      <button
-        className="text-white bg-neutral-800 w-full py-2 rounded-lg enabled:hover:bg-neutral-900 disabled:opacity-50"
-        disabled={file == undefined}
-        onClick={handleSubmit}
-      >
-        Upload
-      </button>
-    </div>
+    </>
   );
 }
--- client/src/components/upload_page/BrowseFile.tsx
+++ client/src/components/upload_page/BrowseFile.tsx
@@ -1,6 +1,12 @@
 import { useRef, useState } from "react";
 
-export default function BrowseFile({ file, onFileSelect }: { file: File | undefined, onFileSelect: (file: File | undefined) => void }) {
+export default function BrowseFile({
+  file,
+  onFileSelect,
+}: {
+  file: File | undefined;
+  onFileSelect: (file: File | undefined) => void;
+}) {
   const inputFile = useRef<HTMLInputElement | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -9,7 +15,7 @@ export default function BrowseFile({ file, onFileSelect }: { file: File | undefi
   };
 
   return (
-    <div className="flex">
+    <>
       <input
         type="file"
         accept="application/pdf"
@@ -18,20 +24,38 @@ export default function BrowseFile({ file, onFileSelect }: { file: File | undefi
         className="hidden"
         onChange={handleFileChange}
       />
-      <input
-        type="text"
-        placeholder="Choose file to upload"
-        value={file ? file.name : ""}
-        className="py-2 px-4 w-full border-2 border-neutral-800 rounded-l-md focus:outline-none"
-        readOnly
-        onClick={() => inputFile.current?.click()}
-      />
-      <button
-        onClick={() => inputFile.current?.click()}
-        className="py-2 px-16 bg-neutral-800 text-white rounded-r-md hover:bg-neutral-900 focus:outline-none"
-      >
-        Browse
-      </button>
-    </div>
+      <div className="sm:flex hidden">
+        <input
+          type="text"
+          placeholder="Choose file to upload"
+          value={file ? file.name : ""}
+          className="py-2 px-4 w-full border-2 border-neutral-800 rounded-l-md focus:outline-none"
+          readOnly
+          onClick={() => inputFile.current?.click()}
+        />
+        <button
+          onClick={() => inputFile.current?.click()}
+          className="py-2 px-16 bg-neutral-800 text-white rounded-r-md hover:bg-neutral-900 focus:outline-none"
+        >
+          Browse
+        </button>
+      </div>
+      <div className="flex flex-col sm:hidden">
+        <input
+          type="text"
+          placeholder="Choose file to upload"
+          value={file ? file.name : ""}
+          className="py-2 px-4 w-full border-2 border-neutral-800 rounded-md focus:outline-none"
+          readOnly
+          onClick={() => inputFile.current?.click()}
+        />
+        <button
+          onClick={() => inputFile.current?.click()}
+          className="mt-2 py-2 px-16 bg-neutral-800 text-white rounded-md hover:bg-neutral-900 focus:outline-none"
+        >
+          Browse
+        </button>
+      </div>
+    </>
   );
 }
