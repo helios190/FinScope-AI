73f7a0f7b3894895ee870411c9963f5bcf0bb1be
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-05-24 23:48:06 +0700

    add ocr_handling

--- server/app.py
+++ server/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask_cors import CORS
 import sys
-
+from dotenv import load_dotenv
+import datetime
 import portofolio_calculation
 import os
+from server.ocr_handle.database import get_db, update_user_data
+from server.ocr_handle.ocr import ocr_image_from_blob
+from server.ocr_handle.storage import upload_file_to_blob, get_blob_client
+
+
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
+
+db = get_db()
+
+
+
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
@@ -34,6 +49,64 @@ def calculate():
     max_sharpe_weights, _ = portofolio_calculation.calculate_max_sharpe_ratio(adj_close_prices, risk_free_rate)
 
     return jsonify({"min_var_portfolios": min_var_portfolios, "max_sharpe_weights": list(max_sharpe_weights),'Alpha & Beta':alpha_beta_dict})
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No file selected for uploading'}), 400
+    
+    try:
+        file_url, filename = upload_file_to_blob(file)
+        
+        # Simpan metadata file di MongoDB
+        document = {
+            "file_name": filename,
+            "file_url": file_url,
+            "upload_date": datetime.datetime.utcnow()
+        }
+        result = db.files.insert_one(document)
+
+        user_data_document = {
+            "file_name": filename,
+            "upload_date": datetime.datetime.utcnow()
+        }
+        user_data_result = db.User_Data.insert_one(user_data_document)
+        
+        return jsonify({
+            'message': 'File successfully uploaded',
+            'file_url': file_url,
+            'document_id': str(result.inserted_id),
+            'user_data_id': str(user_data_result.inserted_id)
+        }), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/ocr_extract', methods=['POST'])
+def ocr_extract():
+    try:
+        last_file = db.User_Data.find_one(sort=[('_id', -1)])
+        
+        if not last_file or 'file_name' not in last_file:
+            return jsonify({'error': 'No file found in the database'}), 400
+        
+        file_name = last_file['file_name']
+        blob_client = get_blob_client(file_name)
+        
+        extracted_text = ocr_image_from_blob(blob_client)
+        update_user_data(last_file['_id'], extracted_text)
+        
+        return jsonify({
+            'message': 'OCR extraction successful',
+            'extracted_text': extracted_text
+        }), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
